ab3a5259a435b14af09e3fac904dbad43dedf137
Jakub Piszczan <jpiszczan@gmail.com>
2020-11-06 00:53:43 +0100

    poprawki

--- scripts/menu.php
+++ scripts/menu.php
@@ -13,7 +13,31 @@
 <nav class="header_nav sticky">
 
     <div class="hnav_element hnav_logo"><span style="color: #444345; background-color: white;">Car</span>Rental</div>
-    <div class="hnav_element hnav4"><a href="scripts/logout-exe.php" class="header_link"><?php echo $sql_user_name_result["Imie"]; ?></a></div>
-            <!--<span class="hnav_element_m">+</span>-->
+    <div class="hnav_element hnav4">
+        <?php if(isset($_SESSION['user']) && !empty($_SESSION['user'])){ echo $sql_user_name_result["Imie"];?>
+            
+            <div class="account_menu">
 
+                <a href="#" class="acc_menu_option">Moje Konto</a>
+                <a href="#" class="acc_menu_option">Zamówienia</a>
+                <a href="scripts/logout-exe.php" class="acc_menu_option">Wyloguj się</a>
+
+            </div>
+
+        <?php 
+
+            }
+            else{
+                echo '<a href="scripts/logout-exe.php">'.$sql_user_name_result["Imie"].'</a>';
+            }
+
+        ?>
+
+    </div>
+            <!--<span class="hnav_element_m">+</span>-->
+        
+
+
+        
 </nav>
+
